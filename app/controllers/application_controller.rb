efb66b3775798683c2e8970e06b4588a046fdead
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-15 14:41:01 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
+  def create
+    User.create(configure_permitted_parameters)
+  end
 
-  private
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true, format: { with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角文字を使用してください' }
+  
+  validates :birthday, presence: true
 end
