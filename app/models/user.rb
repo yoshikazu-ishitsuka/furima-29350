47bebe0331b1ffdca90519e81ab930823e2a9e81
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-16 11:35:49 +0900

    Rubocopチェック済み

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
 
-  VALID_EMAIL_REGEX = /.+@.+/
+  VALID_EMAIL_REGEX = /.+@.+/.freeze
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
-  PASSWORD_REGEX = /[a-z\d]{6,}/i
-  validates :password, presence: true, length: { minimum: 6 }, confirmation: true, format: { with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' }
+  PASSWORD_REGEX = /[a-z\d]{6,}/i.freeze
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true, \
+                       format: { with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' }
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
     validates :last_name
     validates :first_name
   end
-  
+
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角（カタカナ）を使用してください' }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角（カタカナ）を使用してください' }
-  
+
   validates :birthday, presence: true
 end
