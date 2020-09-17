84f3da3eb8e25b97842e2c857d550804546c92d4
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-17 16:19:55 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,18 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @item = @user.items.new(item_params)
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :goods, :details, :category, :status, :shipping_fee_burden, :shipping_area, :days_to_ship, :price, \
+      :image).merge(user_id: current_user.id)
+  end
 end
