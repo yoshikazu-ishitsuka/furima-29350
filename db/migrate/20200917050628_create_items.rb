703b276db664e0a39a5c910345321cfa4f44cbe4
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-17 14:54:10 +0900

    itemsテーブルの作成

--- db/migrate/20200917050628_create_items.rb
+++ db/migrate/20200917050628_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :goods,                 null: false
+      t.text :details,                 null: false
+      t.integer :category,             null: false
+      t.integer :status,               null: false
+      t.integer :shipping_fee_burden,  null: false
+      t.integer :shipping_area,        null: false
+      t.integer :days_to_ship,         null: false
+      t.integer :price,                null: false
+      t.references :user,              null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_023050) do
+ActiveRecord::Schema.define(version: 2020_09_17_050628) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "goods", null: false
+    t.text "details", null: false
+    t.integer "category", null: false
+    t.integer "status", null: false
+    t.integer "shipping_fee_burden", null: false
+    t.integer "shipping_area", null: false
+    t.integer "days_to_ship", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_09_12_023050) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
