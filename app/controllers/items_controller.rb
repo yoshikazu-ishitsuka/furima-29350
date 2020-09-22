6b4354f2a27e233e52740fd2cd047e050ab7ec6a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-22 12:30:34 +0900

    destroyアクション実行時のエラーハンドリングの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -28,8 +28,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.destroy
     redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
