ed5c66d0ee52372a93be774b042ec7fc95201b10
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-23 19:12:44 +0900

    購入ページへの不正なアクセスを無効にする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,21 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index]
+  before_action :move_to_index
+
   def index
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if  user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
 end
