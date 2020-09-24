1bb0c64d8b8dea35b59d27fec454f706885b6c02
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-24 18:00:33 +0900

    PAY.JPの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :set_item, only: [:index]
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_index
 
   def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    # binding.pry
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
@@ -16,6 +29,19 @@ class OrdersController < ApplicationController
     if  user_signed_in? && current_user.id == @item.user.id
       redirect_to root_path
     end
+  end
+
+  def order_params
+    params.require(:order).permit(:token).merge(user_id: current_user.id, item_id: @item.id)
+    # params.require(:order).permit(:user_id, :item_id, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  attr_accessor :token
+  validates :token, presence: true
 end
