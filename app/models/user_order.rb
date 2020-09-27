a412491172888dda778d92aca21aaca5f8941988
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-27 20:18:13 +0900

    商品購入時の電話番号の修正

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -6,7 +6,7 @@ class UserOrder
   validates :postal_code, :prefecture_id, :city_name, :address, :phone_number, :token, presence: true
   validates :prefecture_id, numericality: { only_integer: true }
   validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }, length: { maximum: 8 }
-  validates :phone_number, format: { with: /\d{11}/ }, length: { maximum: 11 }
+  validates :phone_number, format: { with: /\d+/ }, length: { maximum: 11 }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
