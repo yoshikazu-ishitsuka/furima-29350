0462d413258ced018297267a7c500a544cc7cdac
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-27 22:40:16 +0900

    重複していたバリデーションの削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,10 +3,4 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :order
 
-  attr_accessor :token
-
-  validates :token, :postal_code, :prefecture_id, :city_name, :address, :phone_number, presence: true
-  validates :prefecture_id, :phone_number, numericality: { only_integer: true }
-  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }, length: { maximum: 8 }
-  validates :phone_number, format: { with: /\d+/ }, length: { maximum: 11 }
 end
