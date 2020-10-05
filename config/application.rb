df823227db1ebb4fe4fbfb634834605aebab58b6
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-10-05 18:11:50 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Furima29350
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
